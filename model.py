e3a50195dd4d8a39619f19537019caae94fd0a9f
Abdullah Emad <abdullahem1997@hotmail.com>
2019-07-03 15:35:29 +0200

    Combine the backward and forward passes into a model

--- model.py
+++ model.py
@@ -335,3 +335,88 @@ def update_parameters(parameters, grads, learning_rate):
     return parameters    
 
 
+
+def nn_model(X, Y, layers_dims, learning_rate = 0.0075, num_iterations = 3000, print_cost=False):
+    """
+    Implements a L-layer neural network: [LINEAR->RELU]*(L-1)->LINEAR->SIGMOID.
+    
+    Arguments:
+    X -- data, numpy array of shape (num_px * num_px * 3, number of examples)
+    Y -- true "label" vector (containing 0 if cat, 1 if non-cat), of shape (1, number of examples)
+    layers_dims -- list containing the input size and each layer size, of length (number of layers + 1).
+    learning_rate -- learning rate of the gradient descent update rule
+    num_iterations -- number of iterations of the optimization loop
+    print_cost -- if True, it prints the cost every 100 steps
+    
+    Returns:
+    parameters -- parameters learnt by the model. They can then be used to predict.
+    """
+
+
+    np.random.seed(1)
+
+
+    costs = []
+
+
+    parameters = model_init(layers_dims)
+
+    for i in range(num_iterations):
+
+        (AL, caches) = L_model_forward(X, parameters) # Forward pass
+        cost = compute_cost(AL, Y) # compute cost function
+        grads = L_model_backward(AL, Y, caches) # Backward pass
+        parameters = update_parameters(parameters, grads, learning_rate) # update our parameters
+
+
+        if print_cost and i % 100 == 0:
+            print ("Cost after iteration %i: %f" %(i, cost))
+        if print_cost and i % 100 == 0:
+            costs.append(cost)
+
+        # plot the cost
+        plt.plot(np.squeeze(costs))
+        plt.ylabel('cost')
+        plt.xlabel('iterations (per hundreds)')
+        plt.title("Learning rate =" + str(learning_rate))
+        plt.show()
+
+
+def predict(X, parameters):
+    """ 
+    Given a neural network classifies the input as 0 or 1
+    Parameters:
+    X -- the Input to the model
+    Y -- Expected Output
+    parameters -- the neural network parameters
+
+    Returns:
+    AL -- predictions of the model
+    """
+    (AL, caches) = L_model_forward(X, parameters)
+
+    return AL
+
+def calculate_accuracy(X, Y, parameters):
+
+    """ 
+       Given a neural network classifies the input as 0 or 1
+       Parameters:
+       X -- the Input to the model
+       Y -- Expected Output
+       parameters -- the neural network parameters
+       
+       Returns:
+       AL -- predictions of the model
+    """
+    AL = predict(X, parameters)
+
+    misclassified = np.abs(AL - Y)
+    
+    acc = (1 - np.mean(misclassified)) * 100
+
+    print("Accuracy: {} %".format(acc))
+
+
+
+
