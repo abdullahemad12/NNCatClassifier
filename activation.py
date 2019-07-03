da0cf386781aa637ff4033d13115750eb5da7509
Abdullah Emad <abdullahem1997@hotmail.com>
2019-07-03 15:37:55 +0200

    Refactor activation functions into a seperate file

---
+++ activation.py
@@ -0,0 +1,68 @@
+import numpy as np
+
+
+def sigmoid(A):
+    """
+    Arguments:
+    A -- np array containing the input to the sigmoid function
+    
+    Returns:
+    parameters -- np array containing the output of the sigmoid function
+    """
+    Z = 1 / (1 + np.exp(-A))
+    return Z
+
+def relu(A):
+    """
+    Arguments:
+    A -- np array containing the input to the relu function
+    
+    Returns:
+    parameters -- np array containing the output of the relu function
+    """
+     
+    A[A < 0] = 0
+    return A
+
+def relu_backward(dA, cache):
+    """
+    Implement the backward propagation for a single RELU unit.
+
+    Arguments:
+    dA -- post-activation gradient, of any shape
+    cache -- 'Z' where we store for computing backward propagation efficiently
+
+    Returns:
+    dZ -- Gradient of the cost with respect to Z
+    """
+    
+    Z = cache["Z"]
+    dZ = np.array(dA, copy=True) # just converting dz to a correct object.
+    
+    # When z <= 0, you should set dz to 0 as well. 
+    dZ[Z <= 0] = 0
+    
+    assert (dZ.shape == Z.shape)
+    
+    return dZ
+
+
+
+def sigmoid_backward(dA, cache):
+    """
+    Implement the backward propagation for a single SIGMOID unit.
+
+    Arguments:
+    dA -- post-activation gradient, of any shape
+    cache -- 'Z' where we store for computing backward propagation efficiently
+
+    Returns:
+    dZ -- Gradient of the cost with respect to Z
+    """
+    
+    Z = cache["Z"]
+
+    dZ = dA * sigmoid(Z) * (1 - sigmoid(Z))
+
+    return dZ
+
--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+from activation import *
 from testCases_v4 import *
 
 
@@ -31,71 +31,6 @@ def model_init(model_dims):
 
     return dic
 
-
-def sigmoid(A):
-    """
-    Arguments:
-    A -- np array containing the input to the sigmoid function
-    
-    Returns:
-    parameters -- np array containing the output of the sigmoid function
-    """
-    Z = 1 / (1 + np.exp(-A))
-    return Z
-
-def relu(A):
-    """
-    Arguments:
-    A -- np array containing the input to the relu function
-    
-    Returns:
-    parameters -- np array containing the output of the relu function
-    """
-     
-    A[A < 0] = 0
-    return A
-
-def relu_backward(dA, cache):
-    """
-    Implement the backward propagation for a single RELU unit.
-
-    Arguments:
-    dA -- post-activation gradient, of any shape
-    cache -- 'Z' where we store for computing backward propagation efficiently
-
-    Returns:
-    dZ -- Gradient of the cost with respect to Z
-    """
-    
-    Z = cache["Z"]
-    dZ = np.array(dA, copy=True) # just converting dz to a correct object.
-    
-    # When z <= 0, you should set dz to 0 as well. 
-    dZ[Z <= 0] = 0
-    
-    assert (dZ.shape == Z.shape)
-    
-    return dZ
-
-
-
-def sigmoid_backward(dA, cache):
-    """
-    Implement the backward propagation for a single SIGMOID unit.
-
-    Arguments:
-    dA -- post-activation gradient, of any shape
-    cache -- 'Z' where we store for computing backward propagation efficiently
-
-    Returns:
-    dZ -- Gradient of the cost with respect to Z
-    """
-    
-    Z = cache["Z"]
-
-    dZ = dA * sigmoid(Z) * (1 - sigmoid(Z))
-
-    return dZ
 
 def layer_forward(A, W, b):
     """
