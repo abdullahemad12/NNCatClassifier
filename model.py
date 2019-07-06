cc034e66b643d0401f8e5bc64293aa25f6873b7d
Abdullah Emad <abdullahem1997@hotmail.com>
2019-07-06 14:23:49 +0200

    Final Modification to my model

--- model.py
+++ model.py
@@ -279,7 +279,7 @@ def update_parameters(parameters, grads, learning_rate):
 
 
 
-def nn_model(X, Y, layers_dims, learning_rate = 0.01, num_iterations = 3000, print_cost=False):
+def nn_model(X, Y, layers_dims, learning_rate = 0.0075, num_iterations = 3000, print_cost=False):
     """
     Implements a L-layer neural network: [LINEAR->RELU]*(L-1)->LINEAR->SIGMOID.
     
